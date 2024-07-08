cc1e8d3e5b2a496688a690057ab640b823f4e90b
Oleksandr Lotsman <lotsman1993@gmail.com>
2024-07-08 14:16:09 +0400

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
+
 import './App.css';
+
 import Header from './components/Header/Header';
 import Courses from './components/Courses/Courses';
+import EmptyCourseList from './components/EmptyCourseList/EmptyCourseList';
+
 import { mockedAuthorsList, mockedCoursesList } from './constants';
-import EmptyCourseList from "./components/EmptyCourseList/EmptyCourseList";
 
-function defineMainPage() {
-	return mockedCoursesList.length !== 0
-		? <Courses coursesList={mockedCoursesList} authorsList={mockedAuthorsList}/>
-		: <EmptyCourseList/>
-
+const defineMainPage = () => {
+	return mockedCoursesList.length !== 0 ? (
+		<Courses coursesList={mockedCoursesList} authorsList={mockedAuthorsList} />
+	) : (
+		<EmptyCourseList />
+	);
 }
 
 function App() {
 	return (
 		<div className='app'>
-			<Header/>
-			<div className={'main-page'}>
-				{defineMainPage()}
-			</div>
+			<Header />
+			<div className={'main-page'}>{defineMainPage()}</div>
 		</div>
 	);
 }
