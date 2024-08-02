8eb29c6a6e25b112267965d1d418a978cd728588
Oleksandr Lotsman <olotsman@ebsco.com>
2024-08-02 13:59:02 +0400

    Update header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,15 +2,32 @@ import React from 'react';
 
 import './Header.css';
 
-import { Logo } from './components/Logo/Logo';
-import { Button } from '../../common/Button';
+import {Logo} from './components/Logo/Logo';
+import {Button} from '../../common/Button';
 
-import { BUTTON_TEXT_LOGOUT } from '../../constants';
+import {BUTTON_TEXT_LOGOUT} from '../../constants';
+import {useNavigate} from "react-router-dom";
+
 const Header = () => {
+	const token = localStorage.getItem('token');
+	const name = localStorage.getItem('name');
+	const navigate = useNavigate();
+
+	const handleLogout = () => {
+		localStorage.removeItem('token');
+		localStorage.removeItem('name');
+		navigate('/login');
+	}
+
 	return (
 		<header className='header'>
 			<Logo />
-			<Button text={BUTTON_TEXT_LOGOUT} />
+			{token && <div>
+                <span>{name}</span>
+                <Button text={BUTTON_TEXT_LOGOUT} onClick={handleLogout}/>
+            </div>
+			}
+
 		</header>
 	);
 };
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -53,7 +53,9 @@ const Login = () => {
 			return;
 		}
 		const token = responseBody.result;
+		const user: User = responseBody.user
 		localStorage.setItem('token', JSON.stringify(token));
+		localStorage.setItem('name', user.name);
 		navigate('/courses');
 	};
 
